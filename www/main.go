869923ab399061fc909cbd8c8c5cec20fd916f49
Georgy <georgy.polkanov@yandex.ru>
2023-10-03 20:53:13 +0300

    plug in HTTP

--- www/main.go
+++ www/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Go is super cool!")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contacts", contacts_page)
+	http.ListenAndServe(":8000", nil)
+}
 
 func main() {
-	fmt.Println("Hello, world")
+	handleRequest()
 }
